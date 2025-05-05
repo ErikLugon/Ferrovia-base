10ca3ec127619fd6b82eebcc93f8338808c8f6ec
Obelisco <141413429+AnaoIrmao@users.noreply.github.com>
2025-05-05 15:43:22 -0300

    Update falas.py

--- falas.py
+++ falas.py
@@ -1,13 +1,7 @@
-checaOLog = ['Checa o log, primata!', 'Checa o log, galinha depenada',
-             'Yo phone lingin, cmon big boy, pick yo phon',
-             'Checa o log, sabonete de mecânico',
-             'Vem dormir comigo pedro, vem dormir comigo',
+checaOLog = [
              'tem alguém me mandando mensagem, checa o log',
              ]
 
-xingamentos = ['Vai tomar no cu leandro', 
-                   'Odeio judeus',
-                   'Desista dos seus sonhos e morra',
-                   'Me atire aos lobos, e voltarei morto né porrakkkkkkkkkkkkkkkkkkkkkk',
-                   'mimm cago nas kalssah',
+lines = [
+  'Line sample'
     ]
